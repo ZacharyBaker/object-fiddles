bbbfb1b889a910e3241904605e4972a5cbcb49e1
Zach Baker <zach.r.baker1@gmail.com>
2015-10-17 17:57:01 -0600

    prob 2

--- practice.js
+++ practice.js
@@ -20,20 +20,34 @@ alert(me.name);
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following
+// keys: band, food, person, book, movie, holiday. Have the values to
+// those keys be your favorite thing in that category.
 
   //Code here
+var favoriteThings = {
+  band: "Tokyo Police Club",
+  food: "Sushi",
+  book: "Is your mama a llama?",
+  movie: "Forgetting Sarah Marshall",
+  holiday: "Christmas"
+}
 
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+//After you've made your object, add another key
+// named 'car' with the value being your favorite car and then 
+//another key named 'brand' with the value being your favorite brand.
 
   //Code here
+favoriteThings.car = "Subaru";
+favoriteThings.brand = "Ruca";
 
-
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
+//Now change the food key in your favoriteThings 
+//object to be 'Lettuce' and change the book key 
+//in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
-
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 
 
 
